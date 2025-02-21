7a80da27b7f40a281219076bcb9112125fa2b095
roger21 <roger21@free.fr>
2025-02-21 10:42:32 +0100

    hop update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -2303,12 +2303,12 @@
         },
         "pb": 490557,
         "streak": 11,
-        "matches": 2479,
-        "playtime": 1634766589,
+        "matches": 2480,
+        "playtime": 1635990887,
         "finished": 762,
         "finishtime": 621041435,
         "won": 1286,
-        "lost": 1119,
+        "lost": 1120,
         "forfeited": 578
     },
     "879c8136d42c4222b42f1c090616fa5e": {
