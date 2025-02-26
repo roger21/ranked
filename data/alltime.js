3ae44faf6b59b4bc6619269e386067f72c52fcdc
roger21 <roger21@free.fr>
2025-02-26 16:14:03 +0100

    hop update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -1778,13 +1778,13 @@
         },
         "pb": 469419,
         "streak": 10,
-        "matches": 1744,
-        "playtime": 1126516522,
+        "matches": 1745,
+        "playtime": 1127186883,
         "finished": 555,
         "finishtime": 440507776,
         "won": 896,
-        "lost": 774,
-        "forfeited": 307
+        "lost": 775,
+        "forfeited": 308
     },
     "6fbedfe6fbf14495a1b45b98e9f896e5": {
         "nickname": "zidouri",
