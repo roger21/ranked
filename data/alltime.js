90d30c69ab510a028d25e37afc444fdd61d1c074
update_ranked <>
2025-02-27 01:22:34 +0100

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -3949,37 +3949,6 @@
         "lost": 288,
         "forfeited": 45
     },
-    "f2e05ad464b54d288fa18da14e9a2786": {
-        "nickname": "LEC666888",
-        "country": "cn",
-        "top": {
-            "1": 1161,
-            "2": 1536,
-            "3": 1419,
-            "4": 1558,
-            "5": 1627,
-            "6": 1849,
-            "7": 1789
-        },
-        "points": {
-            "1": 0,
-            "2": 0,
-            "3": 0,
-            "4": 0,
-            "5": 20,
-            "6": 30,
-            "7": 5
-        },
-        "pb": 433890,
-        "streak": 14,
-        "matches": 4631,
-        "playtime": 2808979419,
-        "finished": 1408,
-        "finishtime": 1158543342,
-        "won": 2311,
-        "lost": 2127,
-        "forfeited": 1232
-    },
     "fa1bec35058546c98f9279f8be7cf9bc": {
         "nickname": "MoleyG",
         "country": null,
