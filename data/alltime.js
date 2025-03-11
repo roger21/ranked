fc47f4c581303b4ff7ef71bed0f47463ed0e3c0e
update_ranked <>
2025-03-11 04:42:32 +0100

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -802,6 +802,35 @@
         "lost": 272,
         "forfeited": 30
     },
+    "2fe70934e7be458dba747c4ac830391c": {
+        "nickname": "nhb_",
+        "country": "us",
+        "top": {
+            "2": 793,
+            "3": 1302,
+            "4": 1419,
+            "5": 1698,
+            "6": 1985,
+            "7": 1829
+        },
+        "points": {
+            "2": 0,
+            "3": 0,
+            "4": 0,
+            "5": 15,
+            "6": 25,
+            "7": 0
+        },
+        "pb": 438416,
+        "streak": 9,
+        "matches": 3545,
+        "playtime": 2229670738,
+        "finished": 983,
+        "finishtime": 783309381,
+        "won": 1671,
+        "lost": 1588,
+        "forfeited": 786
+    },
     "31f96d84ed9844bcabbd07f828001290": {
         "nickname": "Aquaface",
         "country": null,
