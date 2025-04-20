21e9199947e290a2cedfbb53d2b5eadd71455cd2
update_ranked <>
2025-04-20 16:33:22 +0200

    update_ranked

--- data/oldtime.js
+++ data/oldtime.js
@@ -359,8 +359,8 @@
         "d0a684fdcbe445d5abb39990ae1cfc3a",
         "d7d0b271136647fea7398a444ab51c13",
         "da5e15a7d3c04a84b432defd5a15c910",
-        "dd5fbfd77b35431e97e8e0ec36817f29",
         "e4808bc3e1e347988cbb59b55d723e0f",
+        "ea96ade538b3498d9de431a5aec6ffbf",
         "ef0814ccd4104b03adb8ab3266535e05"
     ]
 }
