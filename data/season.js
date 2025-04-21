3243d926120d7aa8cdd76ca7a801dc2877ed81db
update_ranked <>
2025-04-21 02:40:35 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,4 +1,27 @@
 {
-    "date": 1745195401000,
-    "players": []
+    "date": 1745196001000,
+    "players": [
+        {
+            "uuid": "c206b071634947768ad7f20774cec73c",
+            "nickname": "linksbasher",
+            "stats": {
+                "rank": 1,
+                "elo": 412,
+                "top": 412,
+                "points": 0,
+                "pb": null,
+                "current": 0,
+                "streak": 0,
+                "matches": 5,
+                "playtime": 906836,
+                "finished": 0,
+                "finishtime": 0,
+                "won": 0,
+                "lost": 4,
+                "forfeited": 4
+            },
+            "country": null,
+            "matches": []
+        }
+    ]
 }
