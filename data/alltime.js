25bd67fc671191d5794f5d765917db9324cd6d43
update_ranked <>
2025-04-26 09:10:58 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -3196,6 +3196,37 @@
         "lost": 1205,
         "forfeited": 589
     },
+    "a00913f1079c4b0c9d68d32f57dfbe73": {
+        "nickname": "sevensix_",
+        "country": "hk",
+        "top": {
+            "1": 1248,
+            "2": 1108,
+            "4": 1637,
+            "5": 1710,
+            "6": 1892,
+            "7": 2028,
+            "8": 1586
+        },
+        "points": {
+            "1": 0,
+            "2": 0,
+            "4": 10,
+            "5": 10,
+            "6": 20,
+            "7": 30,
+            "8": 0
+        },
+        "pb": 448289,
+        "streak": 13,
+        "matches": 4573,
+        "playtime": 2762151367,
+        "finished": 1413,
+        "finishtime": 1079995038,
+        "won": 2218,
+        "lost": 2119,
+        "forfeited": 1236
+    },
     "a0a672a0bc194540bc195220dc170dba": {
         "nickname": "JackoWacko62",
         "country": "us",
