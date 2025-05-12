d15723a4fe6e946b7d9860a61798eae409549114
update_ranked <>
2025-05-12 16:51:14 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -615,6 +615,37 @@
         "lost": 134,
         "forfeited": 18
     },
+    "1f844c3aedb34559b537f45771a7a61d": {
+        "nickname": "Bodek",
+        "country": "pl",
+        "top": {
+            "1": 944,
+            "2": 984,
+            "4": 1214,
+            "5": 1324,
+            "6": 1702,
+            "7": 1664,
+            "8": 1628
+        },
+        "points": {
+            "1": 0,
+            "2": 0,
+            "4": 0,
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "8": 0
+        },
+        "pb": 520315,
+        "streak": 10,
+        "matches": 5143,
+        "playtime": 3215831730,
+        "finished": 1250,
+        "finishtime": 1089018249,
+        "won": 2492,
+        "lost": 2427,
+        "forfeited": 1680
+    },
     "1f88c82f9cc84c9885a5d38dd531f4c6": {
         "nickname": "RRed",
         "country": "us",
@@ -2353,13 +2384,13 @@
         },
         "pb": 430515,
         "streak": 11,
-        "matches": 5426,
-        "playtime": 3363691012,
+        "matches": 5427,
+        "playtime": 3363858714,
         "finished": 1942,
         "finishtime": 1409189520,
         "won": 2752,
-        "lost": 2440,
-        "forfeited": 882
+        "lost": 2441,
+        "forfeited": 883
     },
     "7c92678742eb4e819f3122017697ae3d": {
         "nickname": "hackingnoises",
@@ -3040,11 +3071,11 @@
         },
         "pb": 439571,
         "streak": 9,
-        "matches": 4610,
-        "playtime": 2922088587,
-        "finished": 1654,
-        "finishtime": 1223525833,
-        "won": 2310,
+        "matches": 4611,
+        "playtime": 2922756836,
+        "finished": 1655,
+        "finishtime": 1224194082,
+        "won": 2311,
         "lost": 2127,
         "forfeited": 794
     },
@@ -3881,39 +3912,6 @@
         "won": 1260,
         "lost": 1002,
         "forfeited": 242
-    },
-    "c41b14ac0f1547aeb7690276d82aa458": {
-        "nickname": "Psemcovici",
-        "country": "br",
-        "top": {
-            "1": 1719,
-            "2": 1631,
-            "3": 1610,
-            "4": 1761,
-            "5": 1526,
-            "6": 1629,
-            "7": 1719,
-            "8": 1626
-        },
-        "points": {
-            "1": 0,
-            "2": 0,
-            "3": 0,
-            "4": 10,
-            "5": 0,
-            "6": 0,
-            "7": 10,
-            "8": 0
-        },
-        "pb": 508447,
-        "streak": 10,
-        "matches": 2730,
-        "playtime": 1750556355,
-        "finished": 885,
-        "finishtime": 723140758,
-        "won": 1417,
-        "lost": 1252,
-        "forfeited": 652
     },
     "c4ea81fc270c4beb8f1db8afd322b754": {
         "nickname": "nwolc",
