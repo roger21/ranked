184388e51fa7d2249c034106ce646abca9d3c4ae
update_ranked <>
2025-05-26 12:41:40 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -3634,6 +3634,39 @@
         "lost": 1114,
         "forfeited": 315
     },
+    "b0dea0d890de4eecb781481130c3c1d1": {
+        "nickname": "Mixray_",
+        "country": "pl",
+        "top": {
+            "1": 1732,
+            "2": 1580,
+            "3": 1506,
+            "4": 1600,
+            "5": 1524,
+            "6": 1433,
+            "7": 1651,
+            "8": 1711
+        },
+        "points": {
+            "1": 0,
+            "2": 0,
+            "3": 0,
+            "4": 5,
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "8": 8
+        },
+        "pb": 411585,
+        "streak": 12,
+        "matches": 609,
+        "playtime": 415749471,
+        "finished": 201,
+        "finishtime": 164301518,
+        "won": 343,
+        "lost": 244,
+        "forfeited": 62
+    },
     "b39f5648c91d42dfb121655f2aa52970": {
         "nickname": "GiratinaEX",
         "country": "de",
@@ -4210,12 +4243,12 @@
         },
         "pb": 441909,
         "streak": 14,
-        "matches": 5536,
-        "playtime": 3661861630,
+        "matches": 5537,
+        "playtime": 3662624729,
         "finished": 1715,
         "finishtime": 1330746701,
         "won": 2724,
-        "lost": 2543,
+        "lost": 2544,
         "forfeited": 835
     },
     "da1d85337d244f789d60b009e1e2b346": {
