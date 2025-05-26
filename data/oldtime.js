0c413d1d0f997f2e262bbb536cc84d7c7008adfa
update_ranked <>
2025-05-26 19:51:25 +0200

    update_ranked

--- data/oldtime.js
+++ data/oldtime.js
@@ -372,6 +372,7 @@
         "17e787d1d6374f818b294f2319db370d",
         "25349f93cf194f3baeee93d024eccc21",
         "253b53d832ab4bafb5ee0308d5164ccf",
+        "2988fcfbc6b141a497faa915e13b6592",
         "2ef2bfed3d084649b56290328970ace9",
         "388533d5a2ad4b349a31db4738670a4b",
         "3b01d4b4fef14f178b75f05c04dd34ef",
@@ -410,7 +411,6 @@
         "c32cdd142a0147dcb91771319d745194",
         "c7802cb7c30c47aabc1a7ec790ff2260",
         "d0a684fdcbe445d5abb39990ae1cfc3a",
-        "d1718c79c34e4bce93cd688ac5254b58",
         "dd5fbfd77b35431e97e8e0ec36817f29",
         "e4808bc3e1e347988cbb59b55d723e0f",
         "ea96ade538b3498d9de431a5aec6ffbf"
