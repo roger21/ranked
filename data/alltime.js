73053851a9805d2e15c9c2cc7a7ba03413518a97
update_ranked <>
2025-05-29 07:21:30 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -478,39 +478,6 @@
         "lost": 50,
         "forfeited": 9
     },
-    "173d2482653a41e8a45b7381e140677f": {
-        "nickname": "a4102545",
-        "country": "cn",
-        "top": {
-            "1": 1368,
-            "2": 1580,
-            "3": 1603,
-            "4": 1424,
-            "5": 1209,
-            "6": 1564,
-            "7": 1807,
-            "8": 1700
-        },
-        "points": {
-            "1": 0,
-            "2": 0,
-            "3": 0,
-            "4": 0,
-            "5": 0,
-            "6": 0,
-            "7": 15,
-            "8": 3
-        },
-        "pb": 469144,
-        "streak": 11,
-        "matches": 855,
-        "playtime": 564365057,
-        "finished": 274,
-        "finishtime": 220796008,
-        "won": 451,
-        "lost": 328,
-        "forfeited": 88
-    },
     "17e787d1d6374f818b294f2319db370d": {
         "nickname": "silverrruns",
         "country": "ca",
@@ -1477,12 +1444,12 @@
         },
         "pb": 495650,
         "streak": 9,
-        "matches": 1113,
-        "playtime": 776935248,
+        "matches": 1114,
+        "playtime": 777665023,
         "finished": 423,
         "finishtime": 335480162,
         "won": 618,
-        "lost": 472,
+        "lost": 473,
         "forfeited": 116
     },
     "478b956844414cba9cfccb2977b71962": {
@@ -3190,13 +3157,13 @@
         },
         "pb": 459531,
         "streak": 18,
-        "matches": 3636,
-        "playtime": 2344059107,
+        "matches": 3637,
+        "playtime": 2344638410,
         "finished": 1162,
         "finishtime": 917369874,
         "won": 1878,
-        "lost": 1658,
-        "forfeited": 848
+        "lost": 1659,
+        "forfeited": 849
     },
     "9dcb17d831b24df2bae778cfd750ab1b": {
         "nickname": "loodlow",
@@ -4355,6 +4322,33 @@
         "won": 4504,
         "lost": 4230,
         "forfeited": 1031
+    },
+    "dd5fbfd77b35431e97e8e0ec36817f29": {
+        "nickname": "Traden",
+        "country": "tw",
+        "top": {
+            "4": 1087,
+            "5": 1388,
+            "6": 1636,
+            "7": 1902,
+            "8": 1732
+        },
+        "points": {
+            "4": 0,
+            "5": 0,
+            "6": 0,
+            "7": 35,
+            "8": 4
+        },
+        "pb": 482356,
+        "streak": 14,
+        "matches": 6006,
+        "playtime": 3850942716,
+        "finished": 1605,
+        "finishtime": 1356104654,
+        "won": 2974,
+        "lost": 2859,
+        "forfeited": 1770
     },
     "e43dad544b244da9b690a12fdc8626dc": {
         "nickname": "NiceTwice",
