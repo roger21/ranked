2d6b149c365dc366441d52a2c249acc8f379c653
update_ranked <>
2025-06-06 22:11:46 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -720,12 +720,12 @@
         },
         "pb": 429880,
         "streak": 12,
-        "matches": 1736,
-        "playtime": 1118380920,
+        "matches": 1737,
+        "playtime": 1118985853,
         "finished": 800,
         "finishtime": 538256139,
         "won": 980,
-        "lost": 712,
+        "lost": 713,
         "forfeited": 47
     },
     "2549857b8bbf48ad8659ad6c410209a9": {
@@ -4091,6 +4091,29 @@
         "won": 1277,
         "lost": 1086,
         "forfeited": 521
+    },
+    "cc432b2626a44ae1836a50244adbf468": {
+        "nickname": "DziabaDziuba",
+        "country": "pl",
+        "top": {
+            "6": 1636,
+            "7": 1804,
+            "8": 1826
+        },
+        "points": {
+            "6": 0,
+            "7": 40,
+            "8": 8
+        },
+        "pb": 425033,
+        "streak": 27,
+        "matches": 2743,
+        "playtime": 1576981868,
+        "finished": 844,
+        "finishtime": 644546225,
+        "won": 1360,
+        "lost": 1260,
+        "forfeited": 822
     },
     "d0a684fdcbe445d5abb39990ae1cfc3a": {
         "nickname": "danterus",
