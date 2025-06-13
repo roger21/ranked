b8980724ec79255fcbc3e1c04517668ea9bc2bb8
update_ranked <>
2025-06-13 21:51:41 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -4119,29 +4119,6 @@
         "lost": 1086,
         "forfeited": 521
     },
-    "cc432b2626a44ae1836a50244adbf468": {
-        "nickname": "Watermelon1708",
-        "country": "pl",
-        "top": {
-            "6": 1636,
-            "7": 1804,
-            "8": 1852
-        },
-        "points": {
-            "6": 0,
-            "7": 40,
-            "8": 8
-        },
-        "pb": 425033,
-        "streak": 27,
-        "matches": 2866,
-        "playtime": 1649052035,
-        "finished": 889,
-        "finishtime": 674473652,
-        "won": 1423,
-        "lost": 1319,
-        "forfeited": 843
-    },
     "d0a684fdcbe445d5abb39990ae1cfc3a": {
         "nickname": "danterus",
         "country": "us",
