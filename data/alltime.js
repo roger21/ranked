7bf4a7151af676fdd4d131dad10c6c1fa24b6709
update_ranked <>
2025-06-18 06:21:40 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -1743,6 +1743,39 @@
         "lost": 1194,
         "forfeited": 711
     },
+    "529c478ae270415ba12044771a99249a": {
+        "nickname": "mukvl",
+        "country": "in",
+        "top": {
+            "1": 1198,
+            "2": 1173,
+            "3": 1170,
+            "4": 1283,
+            "5": 1311,
+            "6": 1359,
+            "7": 1998,
+            "8": 1825
+        },
+        "points": {
+            "1": 0,
+            "2": 0,
+            "3": 0,
+            "4": 0,
+            "5": 0,
+            "6": 0,
+            "7": 35,
+            "8": 0
+        },
+        "pb": 442239,
+        "streak": 11,
+        "matches": 1840,
+        "playtime": 1148396489,
+        "finished": 621,
+        "finishtime": 457998928,
+        "won": 920,
+        "lost": 839,
+        "forfeited": 300
+    },
     "553414a2c89b4d6b8c0ba5bd89284508": {
         "nickname": "ulsah1n",
         "country": "tr",
@@ -4046,29 +4079,6 @@
         "won": 2568,
         "lost": 1887,
         "forfeited": 1085
-    },
-    "be4ad7e999e14a74974d47ac650062a3": {
-        "nickname": "_bidd",
-        "country": "yt",
-        "top": {
-            "6": 968,
-            "7": 1635,
-            "8": 1816
-        },
-        "points": {
-            "6": 0,
-            "7": 0,
-            "8": 0
-        },
-        "pb": 493130,
-        "streak": 8,
-        "matches": 1168,
-        "playtime": 794700327,
-        "finished": 305,
-        "finishtime": 243071293,
-        "won": 540,
-        "lost": 483,
-        "forfeited": 210
     },
     "be7a79f3331042f19c07230752b26725": {
         "nickname": "dinonuggieboi",
