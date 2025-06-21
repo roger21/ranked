d4f1f225375a78f037f063fd72cecad0c3db2f4d
update_ranked <>
2025-06-22 00:01:41 +0200

    update_ranked

--- data/oldtime.js
+++ data/oldtime.js
@@ -411,7 +411,7 @@
         "c32cdd142a0147dcb91771319d745194",
         "c7802cb7c30c47aabc1a7ec790ff2260",
         "cc432b2626a44ae1836a50244adbf468",
-        "dd5fbfd77b35431e97e8e0ec36817f29",
+        "dd34e44dfe5d4e05923d876b9c34ca5f",
         "e4808bc3e1e347988cbb59b55d723e0f",
         "f2e05ad464b54d288fa18da14e9a2786"
     ]
