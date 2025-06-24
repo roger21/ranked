3fd92cbd4fe6a3ff4f38b1de715bfc1f73b509be
update_ranked <>
2025-06-25 00:01:35 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -445,6 +445,39 @@
         "lost": 50,
         "forfeited": 9
     },
+    "173d2482653a41e8a45b7381e140677f": {
+        "nickname": "a4102545",
+        "country": "cn",
+        "top": {
+            "1": 1368,
+            "2": 1580,
+            "3": 1603,
+            "4": 1424,
+            "5": 1209,
+            "6": 1564,
+            "7": 1807,
+            "8": 1804
+        },
+        "points": {
+            "1": 0,
+            "2": 0,
+            "3": 0,
+            "4": 0,
+            "5": 0,
+            "6": 0,
+            "7": 15,
+            "8": 10
+        },
+        "pb": 469144,
+        "streak": 11,
+        "matches": 911,
+        "playtime": 598192369,
+        "finished": 300,
+        "finishtime": 237716924,
+        "won": 483,
+        "lost": 351,
+        "forfeited": 88
+    },
     "17e787d1d6374f818b294f2319db370d": {
         "nickname": "silverrruns",
         "country": "ca",
@@ -2959,12 +2992,12 @@
         },
         "pb": 403425,
         "streak": 14,
-        "matches": 5264,
-        "playtime": 3353881102,
+        "matches": 5265,
+        "playtime": 3354471192,
         "finished": 1992,
         "finishtime": 1415855539,
         "won": 2664,
-        "lost": 2437,
+        "lost": 2438,
         "forfeited": 589
     },
     "97800bfa7f1c42e19162ea1c2bc7078b": {
