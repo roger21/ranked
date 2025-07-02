200b2f21e402a07f33ae105f0b27668ac6726224
update_ranked <>
2025-07-02 03:31:38 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -757,12 +757,12 @@
         },
         "pb": 470188,
         "streak": 11,
-        "matches": 2795,
-        "playtime": 1886555721,
+        "matches": 2796,
+        "playtime": 1887217090,
         "finished": 1079,
         "finishtime": 812601000,
         "won": 1486,
-        "lost": 1245,
+        "lost": 1246,
         "forfeited": 161
     },
     "2988fcfbc6b141a497faa915e13b6592": {
@@ -2069,6 +2069,39 @@
         "lost": 2287,
         "forfeited": 1065
     },
+    "728fdcab24b249b686c081a7fc53176e": {
+        "nickname": "ContraVz",
+        "country": "ie",
+        "top": {
+            "1": 1384,
+            "2": 1254,
+            "3": 1166,
+            "4": 1718,
+            "5": 1755,
+            "6": 2045,
+            "7": 1958,
+            "8": 1837
+        },
+        "points": {
+            "1": 0,
+            "2": 0,
+            "3": 0,
+            "4": 20,
+            "5": 25,
+            "6": 55,
+            "7": 60,
+            "8": 3
+        },
+        "pb": 439581,
+        "streak": 12,
+        "matches": 4312,
+        "playtime": 2583496401,
+        "finished": 1456,
+        "finishtime": 1076105514,
+        "won": 2124,
+        "lost": 1995,
+        "forfeited": 1159
+    },
     "734a1c6118754829acc234135470152c": {
         "nickname": "yeopgihoney",
         "country": "kr",
@@ -3093,11 +3126,11 @@
         },
         "pb": 439571,
         "streak": 10,
-        "matches": 5122,
-        "playtime": 3216903996,
-        "finished": 1862,
-        "finishtime": 1355361937,
-        "won": 2573,
+        "matches": 5123,
+        "playtime": 3217565365,
+        "finished": 1863,
+        "finishtime": 1356023306,
+        "won": 2574,
         "lost": 2357,
         "forfeited": 871
     },
