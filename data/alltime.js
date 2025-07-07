2b6d2da71acb9bce66f1cac0e5b3af431dce2b9e
update_ranked <>
2025-07-07 14:01:40 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -2703,12 +2703,12 @@
         },
         "pb": 448756,
         "streak": 14,
-        "matches": 2972,
-        "playtime": 2002505211,
+        "matches": 2973,
+        "playtime": 2003219212,
         "finished": 1001,
         "finishtime": 748563444,
         "won": 1455,
-        "lost": 1281,
+        "lost": 1282,
         "forfeited": 73
     },
     "8b6de3393be345fdb8dcf0959f42f457": {
@@ -2937,37 +2937,6 @@
         "won": 1071,
         "lost": 938,
         "forfeited": 67
-    },
-    "91ae361cfa7d480d9031baaaa3fa684f": {
-        "nickname": "MultiPori",
-        "country": "de",
-        "top": {
-            "2": 1450,
-            "3": 1302,
-            "4": 1459,
-            "5": 1670,
-            "6": 1546,
-            "7": 1649,
-            "8": 1836
-        },
-        "points": {
-            "2": 0,
-            "3": 0,
-            "4": 0,
-            "5": 10,
-            "6": 5,
-            "7": 0,
-            "8": 8
-        },
-        "pb": 500702,
-        "streak": 21,
-        "matches": 1272,
-        "playtime": 711774247,
-        "finished": 369,
-        "finishtime": 294003207,
-        "won": 647,
-        "lost": 553,
-        "forfeited": 396
     },
     "92b63a39b36a445fa94c77ae212dcea3": {
         "nickname": "bing_pigs",
@@ -4062,6 +4031,29 @@
         "won": 1362,
         "lost": 1156,
         "forfeited": 552
+    },
+    "cc432b2626a44ae1836a50244adbf468": {
+        "nickname": "Watermelon1708",
+        "country": "pl",
+        "top": {
+            "6": 1636,
+            "7": 1804,
+            "8": 2144
+        },
+        "points": {
+            "6": 0,
+            "7": 40,
+            "8": 20
+        },
+        "pb": 414955,
+        "streak": 27,
+        "matches": 3214,
+        "playtime": 1851478108,
+        "finished": 1043,
+        "finishtime": 777774010,
+        "won": 1604,
+        "lost": 1474,
+        "forfeited": 908
     },
     "d0a684fdcbe445d5abb39990ae1cfc3a": {
         "nickname": "danterus",
