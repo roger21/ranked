dba246674619c3bbb0dde492dfa52e0a57104b10
update_ranked <>
2025-07-15 11:01:35 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -2057,13 +2057,13 @@
         },
         "pb": 469419,
         "streak": 11,
-        "matches": 2611,
-        "playtime": 1665036790,
+        "matches": 2612,
+        "playtime": 1665339509,
         "finished": 866,
         "finishtime": 660843626,
         "won": 1327,
-        "lost": 1171,
-        "forfeited": 431
+        "lost": 1172,
+        "forfeited": 432
     },
     "6fbedfe6fbf14495a1b45b98e9f896e5": {
         "nickname": "zidouri",
