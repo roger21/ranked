2e9046fa6b3c0b47895e9b5a1b71ddb8f1021c18
update_ranked <>
2025-08-27 19:10:56 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -1873,39 +1873,6 @@
         "lost": 1278,
         "forfeited": 732
     },
-    "4eea0516cb0c4c528cefe85448cac786": {
-        "nickname": "blobserr",
-        "country": "vn",
-        "top": {
-            "1": 1466,
-            "3": 1424,
-            "4": 1368,
-            "5": 1143,
-            "6": 1653,
-            "7": 1769,
-            "8": 1581,
-            "9": 1650
-        },
-        "points": {
-            "1": 0,
-            "3": 0,
-            "4": 0,
-            "5": 0,
-            "6": 0,
-            "7": 25,
-            "8": 0,
-            "9": 0
-        },
-        "pb": 523673,
-        "streak": 11,
-        "matches": 1112,
-        "playtime": 802378774,
-        "finished": 369,
-        "finishtime": 302649985,
-        "won": 590,
-        "lost": 476,
-        "forfeited": 6
-    },
     "529c478ae270415ba12044771a99249a": {
         "nickname": "mukvl",
         "country": "in",
@@ -3263,12 +3230,12 @@
         },
         "pb": 441689,
         "streak": 14,
-        "matches": 6204,
-        "playtime": 4213326631,
+        "matches": 6205,
+        "playtime": 4213902090,
         "finished": 2034,
         "finishtime": 1632892917,
         "won": 3112,
-        "lost": 2919,
+        "lost": 2920,
         "forfeited": 724
     },
     "8e662c4655534e00802d563043cd8739": {
@@ -3445,6 +3412,33 @@
         "won": 3009,
         "lost": 2768,
         "forfeited": 657
+    },
+    "97559a8303d44690b85db9e1e1f6764f": {
+        "nickname": "shrddr",
+        "country": "yt",
+        "top": {
+            "5": 1365,
+            "6": 1552,
+            "7": 1611,
+            "8": 1735,
+            "9": 1668
+        },
+        "points": {
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "8": 0,
+            "9": 0
+        },
+        "pb": 454401,
+        "streak": 12,
+        "matches": 1100,
+        "playtime": 727222365,
+        "finished": 322,
+        "finishtime": 253223613,
+        "won": 529,
+        "lost": 476,
+        "forfeited": 125
     },
     "97800bfa7f1c42e19162ea1c2bc7078b": {
         "nickname": "misfity",
