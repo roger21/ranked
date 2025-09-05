181048c69bf05260e7dbae3a3ceecd66868420be
update_ranked <>
2025-09-05 08:21:05 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -3391,6 +3391,33 @@
         "lost": 2841,
         "forfeited": 673
     },
+    "97559a8303d44690b85db9e1e1f6764f": {
+        "nickname": "shrddr",
+        "country": "yt",
+        "top": {
+            "5": 1365,
+            "6": 1552,
+            "7": 1611,
+            "8": 1735,
+            "9": 1790
+        },
+        "points": {
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "8": 0,
+            "9": 0
+        },
+        "pb": 446461,
+        "streak": 12,
+        "matches": 1218,
+        "playtime": 799918761,
+        "finished": 370,
+        "finishtime": 286432908,
+        "won": 585,
+        "lost": 527,
+        "forfeited": 138
+    },
     "97800bfa7f1c42e19162ea1c2bc7078b": {
         "nickname": "misfity",
         "country": "de",
@@ -4962,12 +4989,12 @@
         },
         "pb": 449055,
         "streak": 14,
-        "matches": 6254,
-        "playtime": 3904896777,
+        "matches": 6255,
+        "playtime": 3905431112,
         "finished": 2700,
         "finishtime": 1865985846,
         "won": 3412,
-        "lost": 2759,
+        "lost": 2760,
         "forfeited": 755
     },
     "e4cd494a4e0a423b9988618b86858c96": {
