ed66f8aaf1c1d581a94838c1221f758662d194c1
update_ranked <>
2025-09-21 18:01:31 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -2742,12 +2742,12 @@
         },
         "pb": 472950,
         "streak": 12,
-        "matches": 3587,
-        "playtime": 2173622265,
+        "matches": 3588,
+        "playtime": 2174255428,
         "finished": 1262,
         "finishtime": 967916556,
         "won": 1881,
-        "lost": 1620,
+        "lost": 1621,
         "forfeited": 832
     },
     "7f36b01d1b8f4fbcab4df24a454d6e15": {
@@ -3199,6 +3199,33 @@
         "lost": 1000,
         "forfeited": 68
     },
+    "8fefa2e529bf435691eb90a111248f66": {
+        "nickname": "WokerBomzh123",
+        "country": "ru",
+        "top": {
+            "5": 1098,
+            "6": 1314,
+            "7": 1633,
+            "8": 1715,
+            "9": 1806
+        },
+        "points": {
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "8": 0,
+            "9": 3
+        },
+        "pb": 519100,
+        "streak": 9,
+        "matches": 3822,
+        "playtime": 2353690405,
+        "finished": 1046,
+        "finishtime": 830800439,
+        "won": 1825,
+        "lost": 1752,
+        "forfeited": 1150
+    },
     "92b63a39b36a445fa94c77ae212dcea3": {
         "nickname": "bing_pigs",
         "country": "au",
@@ -3537,12 +3564,12 @@
         },
         "pb": 451504,
         "streak": 11,
-        "matches": 3508,
-        "playtime": 2390433950,
+        "matches": 3509,
+        "playtime": 2390996307,
         "finished": 1251,
         "finishtime": 942299833,
         "won": 1778,
-        "lost": 1571,
+        "lost": 1572,
         "forfeited": 170
     },
     "a20541925b5648e1ae88a141ca37911e": {
@@ -4603,11 +4630,11 @@
         },
         "pb": 441909,
         "streak": 14,
-        "matches": 6945,
-        "playtime": 4522812758,
-        "finished": 2238,
-        "finishtime": 1679996783,
-        "won": 3423,
+        "matches": 6946,
+        "playtime": 4523419350,
+        "finished": 2239,
+        "finishtime": 1680603375,
+        "won": 3424,
         "lost": 3195,
         "forfeited": 965
     },
