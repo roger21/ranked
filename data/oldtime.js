fabcc33b16b2910e12e77604502a5e44ba734fa4
update_ranked <>
2025-09-22 21:41:34 +0200

    update_ranked

--- data/oldtime.js
+++ data/oldtime.js
@@ -441,7 +441,6 @@
         "5cd115f0ec1240659db152406c0984a3",
         "5ee577fdc1af45d3a6fb3e086cc293fb",
         "635f35ee69ed4f0c94ff26ece4818956",
-        "6bf7ffd4b9f04d9fbee0e3c6f5babc43",
         "70eb9286e3e24153a8b37c8f884f1292",
         "7665f76f431b41c6b321bea16aff913b",
         "78a8ec9f99d34371b73decd2a78ff9b0",
@@ -465,6 +464,7 @@
         "d7d0b271136647fea7398a444ab51c13",
         "da5e15a7d3c04a84b432defd5a15c910",
         "e4808bc3e1e347988cbb59b55d723e0f",
-        "e811fb301b1a41ea81c1f481ea93c3be"
+        "e811fb301b1a41ea81c1f481ea93c3be",
+        "fb2bd99d6f664e7b96cc6a24834b9a76"
     ]
 }
