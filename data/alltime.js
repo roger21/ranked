98819944ffd7e0c2784a5a8784ad7ba4de9bcacb
update_ranked <>
2025-11-24 10:52:07 +0100

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -2509,12 +2509,12 @@
         },
         "pb": 424838,
         "streak": 12,
-        "matches": 2493,
-        "playtime": 1671141732,
+        "matches": 2494,
+        "playtime": 1671677817,
         "finished": 940,
         "finishtime": 698651374,
         "won": 1320,
-        "lost": 1050,
+        "lost": 1051,
         "forfeited": 27
     },
     "7447dd83b8bc4b7fb97d3ec316f529dc": {
