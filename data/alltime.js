d7e5b6bd1b1b02861f1a326691cf8afd0825e84b
update_ranked <>
2025-12-18 04:01:37 +0100

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -3946,13 +3946,13 @@
         },
         "pb": 448905,
         "streak": 11,
-        "matches": 4173,
-        "playtime": 2574687629,
+        "matches": 4175,
+        "playtime": 2575572882,
         "finished": 1491,
         "finishtime": 1049712803,
         "won": 2106,
-        "lost": 1921,
-        "forfeited": 558
+        "lost": 1923,
+        "forfeited": 559
     },
     "af22aaab9ee74596a3578bd6345d25b5": {
         "nickname": "priffie",
@@ -4225,6 +4225,31 @@
         "won": 3166,
         "lost": 2366,
         "forfeited": 1260
+    },
+    "be4ad7e999e14a74974d47ac650062a3": {
+        "nickname": "bbiddd",
+        "country": "ca",
+        "top": {
+            "6": 968,
+            "7": 1635,
+            "8": 1894,
+            "9": 2161
+        },
+        "points": {
+            "6": 0,
+            "7": 0,
+            "8": 12,
+            "9": 34
+        },
+        "pb": 438638,
+        "streak": 10,
+        "matches": 2134,
+        "playtime": 1373974711,
+        "finished": 695,
+        "finishtime": 493999978,
+        "won": 1015,
+        "lost": 922,
+        "forfeited": 299
     },
     "be7a79f3331042f19c07230752b26725": {
         "nickname": "dinonuggieboi",
