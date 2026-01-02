df5fb919f96bff2e6088cc0b8f7fbdeb4d590618
update_ranked <>
2026-01-02 03:00:51 +0100

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,4 +1,159 @@
 {
-    "date": 1767315001000,
-    "players": []
+    "date": 1767319201000,
+    "players": [
+        {
+            "uuid": "048de51800794a208de7f01652513c32",
+            "nickname": "q3awreh4yu78i",
+            "stats": {
+                "rank": 1,
+                "elo": 1625,
+                "top": 1625,
+                "points": 0,
+                "pb": 462635,
+                "current": 0,
+                "streak": 3,
+                "matches": 4,
+                "playtime": 2228785,
+                "finished": 3,
+                "finishtime": 1609223,
+                "won": 3,
+                "lost": 1,
+                "forfeited": 0
+            },
+            "country": "cw",
+            "matches": []
+        },
+        {
+            "uuid": "6c4b4e74963d40e3b5a7118f3e0b0dc4",
+            "nickname": "leah_mp3",
+            "stats": {
+                "rank": 2,
+                "elo": 1614,
+                "top": 1614,
+                "points": 0,
+                "pb": 472562,
+                "current": 1,
+                "streak": 1,
+                "matches": 4,
+                "playtime": 2202638,
+                "finished": 2,
+                "finishtime": 1083187,
+                "won": 2,
+                "lost": 1,
+                "forfeited": 0
+            },
+            "country": "ca",
+            "matches": []
+        },
+        {
+            "uuid": "8826e1e6d21b46ecbc5d5246b836f36a",
+            "nickname": "4antoo",
+            "stats": {
+                "rank": 3,
+                "elo": 1596,
+                "top": 1596,
+                "points": 0,
+                "pb": null,
+                "current": 0,
+                "streak": 0,
+                "matches": 4,
+                "playtime": 2006571,
+                "finished": 0,
+                "finishtime": 0,
+                "won": 0,
+                "lost": 3,
+                "forfeited": 0
+            },
+            "country": "it",
+            "matches": []
+        },
+        {
+            "uuid": "fdff6a3e88054664974dbcd30583fe81",
+            "nickname": "Finnitzko",
+            "stats": {
+                "rank": 4,
+                "elo": 1581,
+                "top": 1581,
+                "points": 0,
+                "pb": 595401,
+                "current": 0,
+                "streak": 2,
+                "matches": 4,
+                "playtime": 1894166,
+                "finished": 1,
+                "finishtime": 595401,
+                "won": 2,
+                "lost": 2,
+                "forfeited": 0
+            },
+            "country": "de",
+            "matches": []
+        },
+        {
+            "uuid": "25d4de4fe37248f0bba16b43ca1994bb",
+            "nickname": "Hinart",
+            "stats": {
+                "rank": 5,
+                "elo": 1580,
+                "top": 1580,
+                "points": 0,
+                "pb": 579631,
+                "current": 0,
+                "streak": 2,
+                "matches": 4,
+                "playtime": 2364233,
+                "finished": 2,
+                "finishtime": 1270494,
+                "won": 2,
+                "lost": 2,
+                "forfeited": 0
+            },
+            "country": "ua",
+            "matches": []
+        },
+        {
+            "uuid": "aa756a8da9784c16b9496f5f5fcaba09",
+            "nickname": "Czelco",
+            "stats": {
+                "rank": 6,
+                "elo": 1501,
+                "top": 1501,
+                "points": 0,
+                "pb": 587214,
+                "current": 0,
+                "streak": 2,
+                "matches": 4,
+                "playtime": 2185488,
+                "finished": 1,
+                "finishtime": 587214,
+                "won": 2,
+                "lost": 1,
+                "forfeited": 0
+            },
+            "country": "cz",
+            "matches": []
+        },
+        {
+            "uuid": "1ad3ce1d756144e2b967299789c28030",
+            "nickname": "ILuvCobblestone",
+            "stats": {
+                "rank": 7,
+                "elo": 1391,
+                "top": 1391,
+                "points": 0,
+                "pb": null,
+                "current": 0,
+                "streak": 0,
+                "matches": 4,
+                "playtime": 2628901,
+                "finished": 0,
+                "finishtime": 0,
+                "won": 0,
+                "lost": 3,
+                "forfeited": 0
+            },
+            "country": "ca",
+            "matches": []
+        }
+    ]
 }
