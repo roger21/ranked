9569bebe2652c5b5ef39ca29a197f94a63b997da
update_ranked <>
2026-03-18 14:51:53 +0100

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -2861,12 +2861,12 @@
         },
         "pb": 401764,
         "streak": 14,
-        "matches": 8452,
-        "playtime": 4709163671,
+        "matches": 8453,
+        "playtime": 4709761521,
         "finished": 2737,
         "finishtime": 1715723636,
         "won": 3887,
-        "lost": 3760,
+        "lost": 3761,
         "forfeited": 2128
     },
     "7b5563464fdf4757905e681040791ac0": {
@@ -5009,6 +5009,41 @@
         "won": 685,
         "lost": 580,
         "forfeited": 70
+    },
+    "d10be6f072a34e069882ae04bfe60c98": {
+        "nickname": "Unknwonc",
+        "country": "se",
+        "top": {
+            "1": 1322,
+            "3": 1470,
+            "4": 1486,
+            "5": 1761,
+            "6": 1781,
+            "7": 1875,
+            "8": 1961,
+            "9": 2091,
+            "10": 2104
+        },
+        "points": {
+            "1": 0,
+            "3": 0,
+            "4": 0,
+            "5": 25,
+            "6": 20,
+            "7": 15,
+            "8": 20,
+            "9": 12,
+            "10": 6
+        },
+        "pb": 474940,
+        "streak": 12,
+        "matches": 3789,
+        "playtime": 2299176796,
+        "finished": 1323,
+        "finishtime": 938086553,
+        "won": 1887,
+        "lost": 1759,
+        "forfeited": 651
     },
     "d41f0f3caebe45e1bc6a380fedf54ca9": {
         "nickname": "boosterruns",
