1d7bca173fcebc8cf1248822e8c00339eb1413ff
update_ranked <>
2026-05-27 11:31:46 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -67,13 +67,13 @@
         },
         "pb": 416164,
         "streak": 12,
-        "matches": 8381,
-        "playtime": 5442114655,
+        "matches": 8382,
+        "playtime": 5442702425,
         "finished": 3003,
         "finishtime": 2210783750,
         "won": 4183,
-        "lost": 3927,
-        "forfeited": 855
+        "lost": 3928,
+        "forfeited": 856
     },
     "0478fbcaf4da4cf983e1be6d6e11678c": {
         "nickname": "_KrakerS",
@@ -216,45 +216,6 @@
         "won": 3784,
         "lost": 3566,
         "forfeited": 263
-    },
-    "083f99056a5c4503af326c23bcf8dafe": {
-        "nickname": "benemies",
-        "country": "us",
-        "top": {
-            "1": 1543,
-            "2": 1417,
-            "3": 1422,
-            "4": 1586,
-            "5": 1352,
-            "6": 1441,
-            "7": 1610,
-            "8": 1626,
-            "9": 1973,
-            "10": 2180,
-            "11": 1965
-        },
-        "points": {
-            "1": 0,
-            "2": 0,
-            "3": 0,
-            "4": 0,
-            "5": 0,
-            "6": 0,
-            "7": 0,
-            "8": 0,
-            "9": 0,
-            "10": 13,
-            "11": 0
-        },
-        "pb": 442953,
-        "streak": 12,
-        "matches": 5349,
-        "playtime": 3348112769,
-        "finished": 1624,
-        "finishtime": 1280693437,
-        "won": 2577,
-        "lost": 2404,
-        "forfeited": 1061
     },
     "08476f5847fc4daeba74a2544fc9d65b": {
         "nickname": "Zylenox",
@@ -2557,11 +2518,11 @@
         },
         "pb": 393529,
         "streak": 13,
-        "matches": 3618,
-        "playtime": 2123189800,
+        "matches": 3619,
+        "playtime": 2123521077,
         "finished": 1547,
         "finishtime": 985439647,
-        "won": 1920,
+        "won": 1921,
         "lost": 1566,
         "forfeited": 452
     },
@@ -6209,6 +6170,35 @@
         "won": 3879,
         "lost": 3573,
         "forfeited": 2182
+    },
+    "f0103fdd182548828ba66883fdd05214": {
+        "nickname": "Magmania",
+        "country": "au",
+        "top": {
+            "5": 0,
+            "6": 1257,
+            "7": 1598,
+            "9": 1996,
+            "10": 2199,
+            "11": 1898
+        },
+        "points": {
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "9": 0,
+            "10": 0,
+            "11": 0
+        },
+        "pb": 400010,
+        "streak": 9,
+        "matches": 4742,
+        "playtime": 2900398365,
+        "finished": 1580,
+        "finishtime": 1046820025,
+        "won": 2217,
+        "lost": 2060,
+        "forfeited": 631
     },
     "f1611fd339b1401f84154afee3a5f14a": {
         "nickname": "Blooopy",
