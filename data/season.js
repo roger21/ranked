5908d4526313575bb86e99a7fa6368af05430960
update_ranked <>
2026-07-01 03:22:30 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1782868201000,
+    "date": 1782868801000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -4830,20 +4830,6 @@
                     "oelo": 1593,
                     "ochange": -18,
                     "time": 619262,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1778548530000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "TREASURE",
-                    "result": "won",
-                    "opponent": "subdas",
-                    "elo": 1659,
-                    "change": 27,
-                    "oelo": 1797,
-                    "ochange": -27,
-                    "time": 563318,
                     "forfeited": false,
                     "decayed": false
                 }
@@ -72864,20 +72850,6 @@
                     "oelo": 2196,
                     "ochange": 12,
                     "time": 554818,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1778548627000,
-                    "type": "DESERT_TEMPLE",
-                    "bastion": "TREASURE",
-                    "result": "won",
-                    "opponent": "wethr",
-                    "elo": 1950,
-                    "change": 14,
-                    "oelo": 1753,
-                    "ochange": -14,
-                    "time": 621383,
                     "forfeited": false,
                     "decayed": false
                 }
