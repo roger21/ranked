5ed611c5785ccdef42a0606de1d4b43c43b17276
update_ranked <>
2026-07-04 00:31:45 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1783117201000,
+    "date": 1783117801000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -66835,20 +66835,6 @@
                     "oelo": 2172,
                     "ochange": 16,
                     "time": 583712,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1778797352000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "STABLES",
-                    "result": "won",
-                    "opponent": "loodlow",
-                    "elo": 2054,
-                    "change": 12,
-                    "oelo": 1804,
-                    "ochange": -12,
-                    "time": 466890,
                     "forfeited": false,
                     "decayed": false
                 }
@@ -143406,20 +143392,6 @@
                     "time": 749011,
                     "forfeited": false,
                     "decayed": false
-                },
-                {
-                    "date": 1778797352000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "STABLES",
-                    "result": "lost",
-                    "opponent": "okshey",
-                    "elo": 1804,
-                    "change": -12,
-                    "oelo": 2054,
-                    "ochange": 12,
-                    "time": 466890,
-                    "forfeited": false,
-                    "decayed": false
                 }
             ]
         },
@@ -204118,20 +204090,6 @@
                     "oelo": 1658,
                     "ochange": -20,
                     "time": 635427,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1778797530000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "BRIDGE",
-                    "result": "won",
-                    "opponent": "OG_MineMan",
-                    "elo": 1683,
-                    "change": 20,
-                    "oelo": 1645,
-                    "ochange": -20,
-                    "time": 558136,
                     "forfeited": false,
                     "decayed": false
                 }
