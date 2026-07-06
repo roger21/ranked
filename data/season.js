3322ce28e6ea217c9585d80d42eb486c321de3fa
update_ranked <>
2026-07-06 04:51:53 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1783305601000,
+    "date": 1783306201000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -53277,20 +53277,6 @@
                     "time": 786329,
                     "forfeited": false,
                     "decayed": false
-                },
-                {
-                    "date": 1778986183000,
-                    "type": "BURIED_TREASURE",
-                    "bastion": "STABLES",
-                    "result": "won",
-                    "opponent": "_Hypn0tic",
-                    "elo": 1745,
-                    "change": 19,
-                    "oelo": 1676,
-                    "ochange": -19,
-                    "time": 571381,
-                    "forfeited": false,
-                    "decayed": false
                 }
             ]
         },
@@ -67423,20 +67409,6 @@
                     "oelo": 1891,
                     "ochange": -17,
                     "time": 650752,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1778986129000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "HOUSING",
-                    "result": "lost",
-                    "opponent": "priffie",
-                    "elo": 2005,
-                    "change": -22,
-                    "oelo": 1984,
-                    "ochange": 22,
-                    "time": 508675,
                     "forfeited": false,
                     "decayed": false
                 }
