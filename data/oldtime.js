1086da7a3f2ae2489fd2ffe6f0b8cb09b3d53c0b
update_ranked <>
2026-07-14 19:42:05 +0200

    update_ranked

--- data/oldtime.js
+++ data/oldtime.js
@@ -564,11 +564,11 @@
         "cc432b2626a44ae1836a50244adbf468",
         "cdf98ee293f3483fa3890bb3582dbda3",
         "cf1102ea451241d187aaa62e05cab5c3",
+        "dacf61a7b4f74f9cb83512d7f2fe88a9",
         "dd382293fed04a3e9fa850bb139279fc",
         "de8e3203f8674303ad4a2baa55a15c87",
         "e811fb301b1a41ea81c1f481ea93c3be",
         "eb136e7e26124f8daa99e521da609135",
-        "f2061f886c254191871f635dc2ce80b9",
-        "fb2bd99d6f664e7b96cc6a24834b9a76"
+        "f2061f886c254191871f635dc2ce80b9"
     ]
 }
