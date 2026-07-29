e82a0ad91497d26f229a922d05a6809273ad2684
update_ranked <>
2026-07-29 16:52:00 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1785336002000,
+    "date": 1785336601000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -24335,20 +24335,6 @@
                     "result": "draw",
                     "opponent": null,
                     "elo": 2109,
-                    "change": -5,
-                    "oelo": 0,
-                    "ochange": 0,
-                    "time": 0,
-                    "forfeited": true,
-                    "decayed": true
-                },
-                {
-                    "date": 1781016588000,
-                    "type": null,
-                    "bastion": null,
-                    "result": "draw",
-                    "opponent": null,
-                    "elo": 2114,
                     "change": -5,
                     "oelo": 0,
                     "ochange": 0,
