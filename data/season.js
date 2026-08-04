c348a48064db20e8f634e9446e94caca5ef8d1f5
update_ranked <>
2026-08-04 12:52:12 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1785840001000,
+    "date": 1785840601000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -161394,20 +161394,6 @@
                     "time": 725212,
                     "forfeited": false,
                     "decayed": false
-                },
-                {
-                    "date": 1781520343000,
-                    "type": "SHIPWRECK",
-                    "bastion": "TREASURE",
-                    "result": "lost",
-                    "opponent": "Wayyyyyy_",
-                    "elo": 1558,
-                    "change": -18,
-                    "oelo": 1643,
-                    "ochange": 18,
-                    "time": 679293,
-                    "forfeited": false,
-                    "decayed": false
                 }
             ]
         },
@@ -170245,20 +170231,6 @@
                     "ochange": -15,
                     "time": 643990,
                     "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1781520424000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "STABLES",
-                    "result": "draw",
-                    "opponent": "KED_ty",
-                    "elo": 1770,
-                    "change": 0,
-                    "oelo": 1743,
-                    "ochange": 0,
-                    "time": 643302,
-                    "forfeited": true,
                     "decayed": false
                 }
             ]
