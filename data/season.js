66d825658cc56e66ef424c88d3df6ecdb48c8a9a
update_ranked <>
2026-08-05 08:52:35 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1785912001000,
+    "date": 1785912602000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -152825,20 +152825,6 @@
                     "ochange": -20,
                     "time": 576041,
                     "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1781592050000,
-                    "type": "BURIED_TREASURE",
-                    "bastion": "STABLES",
-                    "result": "lost",
-                    "opponent": "Magmania",
-                    "elo": 1856,
-                    "change": -19,
-                    "oelo": 1924,
-                    "ochange": 19,
-                    "time": 492433,
-                    "forfeited": true,
                     "decayed": false
                 }
             ]
