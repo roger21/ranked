abc75c265351e9515cb4a53e3b775ad2d051db72
update_ranked <>
2026-08-07 03:52:27 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1786066801000,
+    "date": 1786067401000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -32663,20 +32663,6 @@
                     "oelo": 2273,
                     "ochange": 16,
                     "time": 563195,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1781747345000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "HOUSING",
-                    "result": "won",
-                    "opponent": "thecamo6",
-                    "elo": 2147,
-                    "change": 12,
-                    "oelo": 1892,
-                    "ochange": -12,
-                    "time": 439374,
                     "forfeited": false,
                     "decayed": false
                 }
@@ -204081,20 +204067,6 @@
                     "oelo": 1691,
                     "ochange": 28,
                     "time": 703969,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1781747345000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "HOUSING",
-                    "result": "lost",
-                    "opponent": "Feinberg",
-                    "elo": 1892,
-                    "change": -12,
-                    "oelo": 2147,
-                    "ochange": 12,
-                    "time": 439374,
                     "forfeited": false,
                     "decayed": false
                 }
