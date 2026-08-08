3954d82577c2fca20f16a78703472593c8829f95
update_ranked <>
2026-08-08 11:51:55 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1786182002000,
+    "date": 1786182601000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -157198,20 +157198,6 @@
                     "ochange": 21,
                     "time": 472709,
                     "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1781862230000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "STABLES",
-                    "result": "won",
-                    "opponent": "yunusbes",
-                    "elo": 1943,
-                    "change": 16,
-                    "oelo": 1791,
-                    "ochange": -16,
-                    "time": 210404,
-                    "forfeited": true,
                     "decayed": false
                 }
             ]
