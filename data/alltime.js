97a3727bb98b1c243fb843771dc6191c8f2a24ba
update_ranked <>
2026-08-09 12:22:14 +0200

    update_ranked

--- data/alltime.js
+++ data/alltime.js
@@ -916,12 +916,12 @@
         },
         "pb": 467946,
         "streak": 12,
-        "matches": 3816,
-        "playtime": 2645790087,
+        "matches": 3817,
+        "playtime": 2646455221,
         "finished": 1341,
         "finishtime": 1033032126,
         "won": 1859,
-        "lost": 1688,
+        "lost": 1689,
         "forfeited": 199
     },
     "25d4de4fe37248f0bba16b43ca1994bb": {
