90235fa7366b41fa7d8888211e5725ad1e9277b2
update_ranked <>
2026-08-20 09:41:27 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1787211001000,
+    "date": 1787211601000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -47453,20 +47453,6 @@
                     "ochange": -15,
                     "time": 318830,
                     "forfeited": true,
-                    "decayed": false
-                },
-                {
-                    "date": 1782891317000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "TREASURE",
-                    "result": "won",
-                    "opponent": "1viewerrr",
-                    "elo": 2207,
-                    "change": 12,
-                    "oelo": 1953,
-                    "ochange": -12,
-                    "time": 687512,
-                    "forfeited": false,
                     "decayed": false
                 }
             ]
