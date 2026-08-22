85b394d9f8178c041519ed2e17e398fe38861398
update_ranked <>
2026-08-22 21:51:25 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1787427601000,
+    "date": 1787428201000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -13337,20 +13337,6 @@
                     "oelo": 2047,
                     "ochange": -22,
                     "time": 578576,
-                    "forfeited": false,
-                    "decayed": false
-                },
-                {
-                    "date": 1783107822000,
-                    "type": "RUINED_PORTAL",
-                    "bastion": "HOUSING",
-                    "result": "won",
-                    "opponent": "Obotron",
-                    "elo": 2001,
-                    "change": 15,
-                    "oelo": 1822,
-                    "ochange": -15,
-                    "time": 637021,
                     "forfeited": false,
                     "decayed": false
                 }
@@ -93886,20 +93872,6 @@
                     "time": 469487,
                     "forfeited": false,
                     "decayed": false
-                },
-                {
-                    "date": 1783108073000,
-                    "type": "SHIPWRECK",
-                    "bastion": "HOUSING",
-                    "result": "lost",
-                    "opponent": "Phulham",
-                    "elo": 2192,
-                    "change": -19,
-                    "oelo": 2249,
-                    "ochange": 19,
-                    "time": 487635,
-                    "forfeited": false,
-                    "decayed": false
                 }
             ]
         },
@@ -161029,20 +161001,6 @@
                     "time": 0,
                     "forfeited": true,
                     "decayed": true
-                },
-                {
-                    "date": 1783108073000,
-                    "type": "SHIPWRECK",
-                    "bastion": "HOUSING",
-                    "result": "won",
-                    "opponent": "Watermelon1708",
-                    "elo": 2249,
-                    "change": 19,
-                    "oelo": 2192,
-                    "ochange": -19,
-                    "time": 487635,
-                    "forfeited": false,
-                    "decayed": false
                 }
             ]
         },
