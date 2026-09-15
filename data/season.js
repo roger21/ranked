96bbf52ebed5fe0fa093d69402e903e7de156623
update_ranked <>
2026-09-15 10:51:09 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1789461601000,
+    "date": 1789462201000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -50669,7 +50669,7 @@
             "uuid": "a5d83ff042164ff1b862dedc118c1dae",
             "stats": {
                 "rank": 27,
-                "elo": 1910,
+                "elo": 1905,
                 "top": 1915,
                 "points": 0,
                 "pb": 387724,
@@ -50685,6 +50685,20 @@
             },
             "country": "gb",
             "matches": [
+                {
+                    "date": 1789461742000,
+                    "type": null,
+                    "bastion": null,
+                    "result": "draw",
+                    "opponent": null,
+                    "elo": 1905,
+                    "change": -5,
+                    "oelo": 0,
+                    "ochange": 0,
+                    "time": 0,
+                    "forfeited": true,
+                    "decayed": true
+                },
                 {
                     "date": 1789375341000,
                     "type": null,
