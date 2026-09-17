e8cd88dce094acb3ec7091bafa0d3bd366647456
update_ranked <>
2026-09-17 10:21:34 +0200

    update_ranked

--- data/season.js
+++ data/season.js
@@ -1,5 +1,5 @@
 {
-    "date": 1789632601000,
+    "date": 1789633201000,
     "players": [
         {
             "uuid": "635f35ee69ed4f0c94ff26ece4818956",
@@ -83145,7 +83145,7 @@
             "uuid": "7f93d3e698cd4d648accc2a4d46ce119",
             "stats": {
                 "rank": 37,
-                "elo": 1880,
+                "elo": 1875,
                 "top": 1890,
                 "points": 0,
                 "pb": 446600,
@@ -83161,6 +83161,20 @@
             },
             "country": null,
             "matches": [
+                {
+                    "date": 1789632685000,
+                    "type": null,
+                    "bastion": null,
+                    "result": "draw",
+                    "opponent": null,
+                    "elo": 1875,
+                    "change": -5,
+                    "oelo": 0,
+                    "ochange": 0,
+                    "time": 0,
+                    "forfeited": true,
+                    "decayed": true
+                },
                 {
                     "date": 1789546284000,
                     "type": null,
